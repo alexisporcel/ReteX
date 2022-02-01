a3167421a189ea0d4642b6d9c0d86517a901e194
alexisporcel <alexisporcel.ap@gmail.com>
2022-02-01 18:12:46 +0100

    Version modifiéee
    Si t'es d'accord patz, on la merge direct

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,9 @@ function tab(event) {
 	console.log(event.target.name);
 	window.location.assign(`./pages/${event.target.name}.html`)
 }
+
+
+function home() {
+	console.log(event.target.name)
+	window.location.assign('../index.html')
+}
