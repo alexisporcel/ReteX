f58f22f8eed20ac7baf04dc15d988d76c5a40f31
ClemPat <clement.patrizio@outlook.fr>
2022-02-06 15:24:54 +0100

    card management done (must be beautified)

--- scripts/fireStation.js
+++ scripts/fireStation.js
@@ -1,8 +1,34 @@
-function room(event) {
+var lastMouseXpos = 0
+var lastMouseYpos = 0
+
+function room(event, state) {
 	var name = event.target.getAttribute('name');
-	console.log(name);
+	let roomCards = document.getElementsByName('card_' + name)
+	if (state=='on') {roomCards[0].style.visibility = 'visible'}
+	if (state=='out') {roomCards[0].style.visibility = 'hidden'}
 }
 
-function fiche(){
-	window.location.assign(`../pages/caserne/${event.target.name}.html`) ;
-}
+document.addEventListener('mousemove', function(e) {
+	let cards = document.getElementsByClassName('card');
+	for (var i = 0; i < cards.length; i++){
+		let card = cards[i]
+		lastMouseXpos = e.x
+		lastMouseYpos = e.y
+		let top = e.y + window.scrollY + 30;
+		let left = e.x + window.scrollX + 30;
+		card.style.top = top + 'px';
+		card.style.left = left + 'px';
+	}
+});
+
+document.addEventListener('scroll',function(e) {
+	let cards = document.getElementsByClassName('card');
+	for (var i = 0; i < cards.length; i++) {
+		let card = cards[i]
+		let infocard = card.getBoundingClientRect();
+		let top = lastMouseYpos + window.scrollY + 30;
+		let left = lastMouseXpos + window.scrollX + 30;
+		card.style.top = top + 'px';
+		card.style.left = left + 'px';
+	}
+})
