6c27dc7811f128d4797ce274cc4bf961d5b96c93
ClemPat <clement.patrizio@outlook.fr>
2022-02-06 14:58:01 +0100

    localisation CHPT

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -3,14 +3,13 @@ var x = document.getElementById("location");
 getLocation()
 
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+  	if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(showPosition);}
+	else {x.innerHTML = "Geolocation is not supported by this browser.";}
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+	let lat = position.coords.latitude
+	let long = position.coords.longitude
+	if (long>2.352 && long<2.36 && lat>48.85 && lat<48.88) {x.innerHTML = 'CHPT'}
+	else { x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;}
 }
