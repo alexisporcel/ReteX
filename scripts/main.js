3192a7dcae17367a548ddab35fdaf4857a80baac
ClemPat <clement.patrizio@outlook.fr>
2022-02-10 21:44:17 +0100

    Glossary

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,9 @@ function tab(event) {
 function home(){
 	window.location.assign(`../index.html`)
 }
+
+function sendMail(){
+	let subject = 'Suggestion de modification ReteX'
+	let body = ''
+	window.open(`mailto:clement.patrizio@gmail.com?subject=${subject}&body=${body}`);
+}
