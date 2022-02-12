49fe1c992106b8205deebe95efad9cd75f101a5b
ClemPat <clement.patrizio@outlook.fr>
2022-02-12 09:36:12 +0100

    links work better

--- scripts/main.js
+++ scripts/main.js
@@ -9,5 +9,17 @@ function tab(event) {
 }
 
 function home(){
-	window.location.assign(`../index.html`)
+	var path = window.location.pathname;
+	var page = path.split('/').pop();
+	if (page=='index.html') {window.location.assign(`./index.html`)}
+	else {
+		if (path.includes('caserne')) {window.location.assign(`../../index.html`)}
+		else {window.location.assign(`../index.html`)}
+	}
+}
+
+function sendMail(){
+	let subject = 'Suggestion de modification ReteX'
+	let body = ''
+	window.open(`mailto:clement.patrizio@gmail.com?subject=${subject}&body=${body}`);
 }
