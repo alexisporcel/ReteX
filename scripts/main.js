b88e497bcd9a958816ff77911404041dac9a70a2
ClemPat <clement.patrizio@outlook.fr>
2022-03-18 11:06:58 +0100

    password works

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ function home(){
 	// 	if (path.includes('caserne')) {window.location.assign(`/index.html`)}
 	// 	else {window.location.assign(`../index.html`)}
 	// }
-	window.location.assign(`/index.html`)
+	window.location.assign(`/main.html`)
 }
 
 function sendMail(){
@@ -40,4 +40,53 @@ function chicAlaJone(){
 	// 	else {window.location.assign(`./chicAlaJone.html`)}
 	// }
 	window.location.assign(`/pages/chicAlaJone.html`)
+}
+
+// ####################################### Login Form ##########################################################
+init()
+function init(){
+	var path = window.location.pathname
+	var page = path.split("/").pop()
+
+	if (page == 'index.html') {
+		document.querySelector('.Password').addEventListener('keypress', function (e) {if (e.key === 'Enter') {submit()}});
+		storage = window.sessionStorage;
+	}
+	else{
+		storage = window.sessionStorage;
+		try { if(storage['auth'] != 'true'){
+			window.location.assign(`/index.html`) } }
+		catch (e) { window.location.assign(`/index.html`) }
+	}
+}
+
+function submit(){
+	mdp = document.getElementsByClassName('Password')[0].value
+	if (sha1(mdp) != '8758d713a95837b1599c17749ac7f43602725681'){logout()}
+	if (sha1(mdp) == '8758d713a95837b1599c17749ac7f43602725681'){login()}
+}
+
+function logout(){
+	sessionStorage.setItem('auth', 'false');
+	window.location.assign('/index.html')
+}
+
+function login(){
+	sessionStorage.setItem('auth', 'true');
+	window.location.assign('/main.html')
+}
+
+function showPassword() {
+  var x = document.getElementsByClassName("Password")[0];
+  var show = document.getElementById("show")
+  var hide = document.getElementById("hide")
+  if (x.type === "password") {
+    x.type = "text";
+	show.style.display = 'none'
+	hide.style.display = 'block'
+  } else {
+    x.type = "password";
+	show.style.display = 'block'
+	hide.style.display = 'none'
+  }
 }
